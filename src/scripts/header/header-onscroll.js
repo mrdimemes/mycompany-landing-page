346a7c896c3c7b44eb46b1bdaf82295066ceb0b8
Dmitry Shagurov <shagurovda@gmail.com>
2022-02-19 20:06:06 +0300

    header nav auto hide on scroll

--- src/scripts/header/header-onscroll.js
+++ src/scripts/header/header-onscroll.js
@@ -1,8 +1,11 @@
 const headerOnScroll = () => {
   const header = document.querySelector(".header");
   const headerNav = document.querySelector(".header__nav");
+  const popupIcon = document.querySelector(".header__popup-icon");
 
   const handleScroll = () => {
+    headerNav.classList.remove("header__nav_active");
+    popupIcon.classList.remove("header__popup-icon_active");
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
       header.classList.add("header_scrolled");
       headerNav.classList.add("header__nav_scrolled");
